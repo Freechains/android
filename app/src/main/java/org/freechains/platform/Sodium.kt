a3b7a8436982fc291d80255abc06d135979ce5df
fsantanna <francisco.santanna@gmail.com>
2020-02-16 15:33:08 -0300

    (+) libsodium

---
+++ app/src/main/java/org/freechains/platform/Sodium.kt
@@ -0,0 +1,6 @@
+package org.freechains.platform
+
+import com.goterl.lazycode.lazysodium.LazySodiumAndroid
+import com.goterl.lazycode.lazysodium.SodiumAndroid
+
+val lazySodium: LazySodiumAndroid = LazySodiumAndroid(SodiumAndroid())
