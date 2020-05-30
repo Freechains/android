b9f5d98deece2627f5c0da611064e7f1cbd32ce9
fsantanna <francisco.santanna@gmail.com>
2020-05-30 12:15:43 -0300

    (>) single platform file

--- app/src/main/java/org/freechains/platform/Sodium.kt
+++
@@ -1,6 +0,0 @@
-package org.freechains.platform
-
-import com.goterl.lazycode.lazysodium.LazySodiumAndroid
-import com.goterl.lazycode.lazysodium.SodiumAndroid
-
-val lazySodium: LazySodiumAndroid = LazySodiumAndroid(SodiumAndroid())
